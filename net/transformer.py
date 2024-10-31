ddc75583a1e7d88178e3b833b04ec5472f0615b0
JackJokeJob <34733988+yyx19941129@users.noreply.github.com>
2024-11-01 00:05:12 +0800

    优化transformer的Feed-Forward层 增加classification层数

--- net/transformer.py
+++ net/transformer.py
@@ -6,14 +6,26 @@ from init import config
 
 
 class ClassificationHead(torch.nn.Module):
-    def __init__(self, embedding_dim, num_classes):
+    def __init__(self, embedding_dim, hidden_size, num_classes):
         super(ClassificationHead, self).__init__()
-        self.fc = torch.nn.Linear(embedding_dim, num_classes)
+        self.fc1 = torch.nn.Linear(embedding_dim, hidden_size)  # 中间层
+        self.norm = torch.nn.LayerNorm(hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(hidden_size, num_classes)
+
+        # 初始化
+        for param in self.parameters():
+            if param.dim() > 1:
+                torch.nn.init.xavier_uniform_(param)
 
     def forward(self, x):
         # 取序列的第一个位置作为分类输入（也可以取平均或最大池化）
-        x = x[:, 0, :]  # [batch_size, embedding_dim]
-        x = self.fc(x)  # [batch_size, num_classes]
+        # x = x[:, 0, :]  # [batch_size, embedding_dim]
+        x = x.max(dim=1)[0]  # [batch_size, embedding_dim]
+        x = self.fc1(x.view(x.shape[0], -1))  # [batch_size, hidden_size]
+        x = self.norm(x)  # [batch_size, hidden_size]
+        x = self.relu(x)  # [batch_size, hidden_size]
+        x = self.fc2(x)  # [batch_size, num_classes]
         return x
 
 
@@ -36,31 +48,38 @@ class PositionalEncoding(torch.nn.Module):
 class TransformerEncoderLayer(torch.nn.Module):
     def __init__(self, embedding_dim, num_heads, dim_feedforward=2048, dropout_probs=0.):
         super().__init__()
-        self.self_attn = torch.nn.MultiheadAttention(embedding_dim, num_heads, dropout=dropout_probs)
-        self.linear1 = torch.nn.Linear(embedding_dim, dim_feedforward)
-        self.dropout = torch.nn.Dropout(dropout_probs)
-        self.linear2 = torch.nn.Linear(dim_feedforward, embedding_dim)
-
+        self.self_attention = torch.nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=num_heads,
+                                                          dropout=dropout_probs)
         self.norm1 = torch.nn.LayerNorm(embedding_dim)
-        self.norm2 = torch.nn.LayerNorm(embedding_dim)
-        self.dropout1 = torch.nn.Dropout(dropout_probs)
-        self.dropout2 = torch.nn.Dropout(dropout_probs)
 
-        self.activation = torch.nn.ReLU()
+        self.feed_forward = torch.nn.Sequential(
+            torch.nn.Linear(embedding_dim, dim_feedforward),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(dropout_probs),
+            torch.nn.Linear(dim_feedforward, embedding_dim),
+            torch.nn.Dropout(dropout_probs),
+        )
+        self.norm2 = torch.nn.LayerNorm(embedding_dim)
+
         for param in self.parameters():
             if param.dim() > 1:
                 torch.nn.init.xavier_uniform_(param)
 
     def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        # Query embeddings of shape :math:(L目标序列长度, N批处理大小, E_qv嵌入维度) when batch_first=False or :math:(N, L, E_q) when batch_first=True
+        # src_key_padding_mask shape (N批处理大小, S源序列长度) [batch_size, seq_len]
+
         # Multi-Head Self-Attention
-        src2 = self.self_attn(src, src, src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
+        sa_out, attn_weights = self.self_attention(src, src, src, attn_mask=src_mask,
+                                                   key_padding_mask=src_key_padding_mask)
+        src = sa_out + src  # Residual Connection
+        src = self.norm1(src)  # 归一化
 
         # Feed-Forward Network
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
+        ff_out = self.feed_forward(src)
+        src = ff_out + src  # Residual Connection
+        src = self.norm2(src)  # 归一化
+
         return src
 
 
@@ -78,7 +97,8 @@ class TransformerEncoder(torch.nn.Module):
         self.layers = torch.nn.ModuleList(
             [TransformerEncoderLayer(embedding_dim, num_heads, dim_feedforward, dropout_probs) for _ in range(nlayers)])
         self.embedding_dim = embedding_dim
-        self.classifier = ClassificationHead(embedding_dim, num_classes)
+        self.classifier = ClassificationHead(embedding_dim=embedding_dim, hidden_size=dim_feedforward,
+                                             num_classes=num_classes)
         # 初始化
         for param in self.parameters():
             if param.dim() > 1:
@@ -87,19 +107,19 @@ class TransformerEncoder(torch.nn.Module):
     def forward(self, src):
         src_mask = None  # Decoder
 
-        # 使用 <PAD> 标记mask
         # src_key_padding_mask = (src == 0)
-        src_key_padding_mask = (src == self.encoder.key_to_index[config.padding_word])
+        # 使用 <PAD> 标记mask
+        src_key_padding_mask = (src == self.encoder.key_to_index[config.padding_word])  # shape: [batch_size, seq_len]
 
         # Embedding and Positional Encoding
         # src = self.encoder.vectors[src] * math.sqrt(self.embedding_dim)
-        src = self.encoder.vectors[src]
+        src = self.encoder.vectors[src]  # shape: [batch_size, seq_len, embedding_dim]
         # 应用位置编码
-        src = self.pos_encoder(src)
+        src = self.pos_encoder(src)  # shape: [batch_size, seq_len, embedding_dim]
 
         # Pass through the encoder layers
         # Convert from [batch_size, seq_len, embedding_dim] to [seq_len, batch_size, embedding_dim]
-        output = src.transpose(0, 1)
+        output = src.transpose(0, 1)  # shape: [seq_len, batch_size, embedding_dim]
         for layer in self.layers:
             output = layer(output, src_mask=src_mask, src_key_padding_mask=src_key_padding_mask)
 
